a5c4e3f0e937821bb1a29eb7a96f2f8c51ef4423
Jordi de Jager <admin@dejagertechnology.com>
2025-12-19 20:30:33 +0000

    moet ik niet wat instellen in firebase of iets wat foutmelding blijft te

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -1,20 +1,30 @@
 'use client';
 
-import { ReactNode, createContext, useContext } from 'react';
+import { ReactNode, createContext, useContext, useEffect, useState } from 'react';
 import { app, db } from './client';
 import { FirebaseApp } from 'firebase/app';
 import { Firestore } from 'firebase/firestore';
 
 interface FirebaseContextType {
-  app: FirebaseApp;
-  db: Firestore;
+  app: FirebaseApp | null;
+  db: Firestore | null;
 }
 
-const FirebaseContext = createContext<FirebaseContextType | null>(null);
+const FirebaseContext = createContext<FirebaseContextType>({ app: null, db: null });
 
 export function FirebaseProvider({ children }: { children: ReactNode }) {
+  const [firebase, setFirebase] = useState<FirebaseContextType>({ app, db });
+
+  // This is a bit of a trick to make sure the firebase object is available on the client side
+  // but doesn't cause hydration issues.
+  useEffect(() => {
+    if (!firebase.app || !firebase.db) {
+      setFirebase({ app, db });
+    }
+  }, [firebase]);
+
   return (
-    <FirebaseContext.Provider value={{ app, db }}>
+    <FirebaseContext.Provider value={firebase}>
       {children}
     </FirebaseContext.Provider>
   );
