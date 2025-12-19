feca86142211c1d88729b511c914ccd61bbcb334
Jordi de Jager <admin@dejagertechnology.com>
2025-12-19 19:37:56 +0000

    En nu nog captan, ureum en regalis plus zichtbaar maken aub

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -19,8 +19,10 @@ export const middelMatrix: Middel[] = [
   { product: 'Captan', crop: 'Appel', disease: 'Schurft', maxDosage: 1.8, unit: 'kg' },
   { product: 'Regalis Plus', crop: 'Appel', maxDosage: 1.25, unit: 'kg' },
   { product: 'Regalis Plus', crop: 'Peer', maxDosage: 1.0, unit: 'kg' },
+  { product: 'Ureum', crop: 'Peer', maxDosage: 5.0, unit: 'kg' },
+  { product: 'Ureum', crop: 'Appel', maxDosage: 5.0, unit: 'kg' },
   { product: 'Zwavel', crop: 'Appel', disease: 'Meeldauw', maxDosage: 7.5, unit: 'kg' },
   { product: 'Koper', crop: 'Appel', disease: 'Kanker', maxDosage: 0.5, unit: 'l' },
 ];
 
-export const products: string[] = ['Captan', 'Regalis Plus', 'Zwavel', 'Koper'];
+export const products: string[] = ['Captan', 'Regalis Plus', 'Zwavel', 'Koper', 'Ureum'];
